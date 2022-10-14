d20c89a5835613e224127447c07d880bbcb09116
almouro <contact@almouro.com>
2022-10-14 14:55:03 +0200

    Install react-navigation

--- FabricEnabled/android/app/src/main/java/com/fabricenabled/MainActivity.java
+++ FabricEnabled/android/app/src/main/java/com/fabricenabled/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fabricenabled;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "FabricEnabled";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
